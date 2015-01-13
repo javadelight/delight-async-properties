eca4416b980da77c8f5f8e91f6625cecf85b8090
mxro <mxro@nowhere.com>
2015-01-14 08:09:52 +1100

    Implementing first version of async-properties by using classes from lightweight java metrics Autocommit Wed Jan 14 08:09:52 EST 2015

--- src/main/java/de/mxro/async/properties/internal/DefaultFactory.java
+++ src/main/java/de/mxro/async/properties/internal/DefaultFactory.java
@@ -1,6 +1,7 @@
 package de.mxro.async.properties.internal;
 
 import de.mxro.async.properties.PropertyFactory;
+import de.mxro.async.properties.values.ObjectValue;
 import de.mxro.async.properties.values.v01.ObjectValueData;
 
 public class DefaultFactory implements PropertyFactory {
@@ -10,6 +11,10 @@ public class DefaultFactory implements PropertyFactory {
     public <T> T create(final Class<T> type) {
 
         if (type.equals(ObjectValueData.class)) {
+            return (T) new ObjectValueData();
+        }
+
+        if (type.equals(ObjectValue.class)) {
             return (T) new ObjectValueData();
         }
 
