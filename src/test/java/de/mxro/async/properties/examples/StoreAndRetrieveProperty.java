5e559c2668189815a6239955edb0f5294e54d980
mxro <mxro@nowhere.com>
2015-01-14 09:19:54 +1100

    Implementing first version of async-properties by using classes from lightweight java metrics Autocommit Wed Jan 14 09:19:54 EST 2015

---
+++ src/test/java/de/mxro/async/properties/examples/StoreAndRetrieveProperty.java
@@ -0,0 +1,10 @@
+package de.mxro.async.properties.examples;
+
+public class StoreAndRetrieveProperty {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
