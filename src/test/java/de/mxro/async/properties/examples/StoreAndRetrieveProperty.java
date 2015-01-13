b9437ba49d2080e023a61caa0992e1651b385da4
mxro <mxro@nowhere.com>
2015-01-14 09:20:54 +1100

    Implementing first version of async-properties by using classes from lightweight java metrics Autocommit Wed Jan 14 09:20:54 EST 2015

--- src/test/java/de/mxro/async/properties/examples/StoreAndRetrieveProperty.java
+++ src/test/java/de/mxro/async/properties/examples/StoreAndRetrieveProperty.java
@@ -1,9 +1,19 @@
 package de.mxro.async.properties.examples;
 
+import de.mxro.async.properties.PropertyNode;
+import de.mxro.async.properties.jre.Properties;
+
 public class StoreAndRetrieveProperty {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public static void main(final String[] args) {
+
+        final PropertyNode props = Properties.create(Properties.defaultFactory());
+
+        props.record(Properties.set("key", "value"));
+
+        props.print();
+
+        props.stop().get();
 
     }
 
