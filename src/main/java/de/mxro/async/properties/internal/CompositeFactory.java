5cb109ae96107d976976aa24ff2a1cd1f5ecde21
mxro <mxro@nowhere.com>
2015-01-14 08:02:52 +1100

    Implementing first version of async-properties by using classes from lightweight java metrics Autocommit Wed Jan 14 08:02:52 EST 2015

--- src/main/java/de/mxro/async/properties/internal/CompositeFactory.java
+++ src/main/java/de/mxro/async/properties/internal/CompositeFactory.java
@@ -1,5 +1,23 @@
 package de.mxro.async.properties.internal;
 
-public class CompositeFactory {
+import java.util.LinkedList;
+import java.util.List;
+
+import de.mxro.async.properties.PropertyFactory;
+
+public class CompositeFactory implements PropertyFactory {
+
+    private final List<PropertyFactory> factories;
+
+    @Override
+    public <T> T create(final Class<T> type) {
+
+        throw new RuntimeException("Cannot instante property of type [" + type + "]");
+    }
+
+    public CompositeFactory() {
+        super();
+        this.factories = new LinkedList<PropertyFactory>();
+    }
 
 }
