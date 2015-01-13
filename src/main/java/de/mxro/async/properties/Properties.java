8f839123057ea47294ee5419833589bd28e494ff
mxro <mxro@nowhere.com>
2015-01-14 07:50:55 +1100

    Implementing first version of async-properties by using classes from lightweight java metrics Autocommit Wed Jan 14 07:50:55 EST 2015

--- src/main/java/de/mxro/async/properties/Properties.java
+++ src/main/java/de/mxro/async/properties/Properties.java
@@ -1,5 +1,11 @@
 package de.mxro.async.properties;
 
+import de.mxro.async.properties.internal.UnsafePropertyNode;
+
 public class Properties {
+
+    public static PropertyNode createUnsafe(final PropertyFactory factory) {
+        return new UnsafePropertyNode(factory);
+    }
 
 }
