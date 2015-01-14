971ef287643cf9a48de23d0d72cc7686d038691b
mxro <mxro@nowhere.com>
2015-01-14 15:05:05 +1100

    Adding documentation for async-log Autocommit Wed Jan 14 15:05:05 EST 2015

--- src/main/java/de/mxro/async/properties/internal/operations/SetValueOperation.java
+++ src/main/java/de/mxro/async/properties/internal/operations/SetValueOperation.java
@@ -4,16 +4,18 @@ import de.mxro.async.properties.PropertyData;
 import de.mxro.async.properties.operations.PropertyOperationWithId;
 import de.mxro.async.properties.values.v01.ObjectValueData;
 
-public class SetValueOperation extends PropertyOperationWithId {
+public class SetValueOperation extends PropertyOperationWithId<Object> {
 
     private final Object value;
 
     @Override
-    public void perform(final PropertyData data) {
+    public Object perform(final PropertyData data) {
 
         final ObjectValueData valueProp = data.get(id, ObjectValueData.class);
 
         valueProp.value = value;
+
+        return value;
 
     }
 
