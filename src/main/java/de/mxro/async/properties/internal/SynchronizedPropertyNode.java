07059472f0e89e5518250cdb0fc1822713ec905d
mxro <mxro@nowhere.com>
2015-01-15 07:25:18 +1100

    Adding documentation for async-log Autocommit Thu Jan 15 07:25:18 EST 2015

--- src/main/java/de/mxro/async/properties/internal/SynchronizedPropertyNode.java
+++ src/main/java/de/mxro/async/properties/internal/SynchronizedPropertyNode.java
@@ -7,7 +7,7 @@ import de.mxro.async.properties.PropertyNode;
 import de.mxro.async.properties.PropertyOperation;
 import de.mxro.concurrency.schedule.AccessThread;
 import de.mxro.concurrency.schedule.Step;
-import de.mxro.fn.Fn;
+import de.mxro.fn.Closure;
 import de.mxro.fn.Success;
 import de.mxro.promise.Promise;
 import de.mxro.promise.helper.PromiseFactory;
@@ -38,7 +38,13 @@ public class SynchronizedPropertyNode implements PropertyNode {
             }
         });
 
-        promise.get(Fn.emptyClosure());
+        promise.get(new Closure<R>() {
+
+            @Override
+            public void apply(final R o) {
+
+            }
+        });
 
         return promise;
 
